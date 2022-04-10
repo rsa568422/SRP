ca58a61b5b2348a3ef53bec2b72c2bc32e22576d
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2022-04-10 14:46:18 +0200

    Fin de la práctica.
    Se realizan los cambios necesarios en el proyecto para cumplir el SRP.

---
+++ src/main/java/srp/refactored/Page.java
@@ -0,0 +1,14 @@
+package srp.refactored;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Page {
+
+    private Integer number;
+
+    private String content;
+
+}
